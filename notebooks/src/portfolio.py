3c4ea24a2d981272535187ca42392665d3e86520
Sampat Ponnaganti <sampat.p@gmail.com>
2020-01-06 20:32:29 -0500

    Displays covariance charts

--- notebooks/src/portfolio.py
+++ notebooks/src/portfolio.py
@@ -7,6 +7,8 @@ from src import asset, logger
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
 
 class engine(object):
     """Updates portfolio PNL"""
@@ -26,11 +28,11 @@ class engine(object):
         data_df = None
         for sravzid in sravzids:
             try:
-                price_df = self.ae.get_historical_price(sravzid)[['Date', 'Last']]
+                price_df = self.ae.get_historical_price(sravzid)[['Last']]
                 #bfill price data: price is in descending order of data
                 price_df['Last'] = price_df['Last'].pct_change(periods=1, fill_method='bfill')
                 #rename columns: cannot have multiple Last column in the same data frame
-                price_df = price_df.rename(columns={'Date': 'Date', 'Last': '%s_last_pct_chg' % (sravzid)})
+                price_df = price_df.rename(columns={'Last': '%s_last_pct_chg' % (sravzid)})
                 # Append all the price dataframe to an array
                 data_dfs.append(price_df)
             except Exception:
@@ -62,19 +64,13 @@ class engine(object):
         '''
         return ((1+self.get_portfolio_assets_daily_returns(name, user_id, portfolio_assets=portfolio_assets)).cumprod()-1)
 
-    def get_portfolio_daily_returns(self, name, user_id, portfolio_assets=None):
+    def get_portfolio_daily_returns(self, portfolio_assets_returns_df):
         '''
             returns portfolio daily returns df
         '''
-        if not portfolio_assets:
-            returns = self.pcae_engine.get_percent_daily_returns(
-                self.util.get_portfolio_assets(name, user_id))
-        else:
-            returns = self.pcae_engine.get_percent_daily_returns(
-                portfolio_assets)
-        numstocks = len(returns.columns)
+        numstocks = len(portfolio_assets_returns_df.columns)
         portfolio_weights_ew = np.repeat(1/numstocks, numstocks)
-        return returns.iloc[:, 0:numstocks].mul(portfolio_weights_ew, axis=1).sum(axis=1)
+        return portfolio_assets_returns_df.iloc[:, 0:numstocks].mul(portfolio_weights_ew, axis=1).sum(axis=1)
 
     def get_portfolio_cummulative_returns(self, name, user_id, portfolio_assets=None):
         '''
@@ -82,3 +78,41 @@ class engine(object):
         '''
         return ((1+self.get_portfolio_daily_returns(name, user_id, portfolio_assets=portfolio_assets)).cumprod()-1)
 
+    def create_portfolio_correlation_report(self, portofolio_assets_daily_returns,
+                                            portofolio_daily_returns,
+                                            return_fig = False):
+        vertical_sections = 5
+        no_of_assets = len(portofolio_assets_daily_returns.columns)
+        widthInch = 10 + no_of_assets * 3
+        heightInch = vertical_sections * 5 + no_of_assets * 3
+        fig = plt.figure(figsize=(widthInch, heightInch))
+
+        gs = gridspec.GridSpec(vertical_sections, 4, wspace=1, hspace=0.5)
+        gs.update(left=0.1, right=0.9, top=0.965, bottom=0.03)
+
+        i = 0
+        ax_cov_matrix = plt.subplot(gs[i, 1:])
+        cov_matrix = portofolio_assets_daily_returns.cov().round(5)
+        #ax2 = fig.add_subplot(122)
+        font_size = 14
+        bbox = [0, 0, 1, 1]
+        ax_cov_matrix.axis('off')
+        mpl_table = ax_cov_matrix.table(
+            cellText=cov_matrix.values, rowLabels=cov_matrix.index, bbox=bbox, colLabels=cov_matrix.columns)
+        mpl_table.auto_set_font_size(False)
+        mpl_table.set_fontsize(font_size)
+        ax_cov_matrix.set_title("Covariance matrix last % daily returns")
+
+        i = 1
+        ax_corr_matrix = plt.subplot(gs[i, 1:])
+        corr_matrix = portofolio_assets_daily_returns.corr().round(5)
+        #ax2 = fig.add_subplot(122)
+        font_size = 14
+        bbox = [0, 0, 1, 1]
+        ax_corr_matrix.axis('off')
+        mpl_table = ax_corr_matrix.table(
+            cellText=corr_matrix.values, rowLabels=corr_matrix.index, bbox=bbox, colLabels=corr_matrix.columns)
+        #mpl_table.set_xticklabels(corr_matrix.index, rotation=45)
+        mpl_table.auto_set_font_size(False)
+        mpl_table.set_fontsize(font_size)
+        ax_corr_matrix.set_title("Correlation matrix last % daily returns")
